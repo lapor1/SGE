9304ad55a65341e975da8f1d57ac68dea62e43af
Patricio Corbelleri <corbelleri.patricio@gmail.com>
2024-05-10 02:19:58 -0300

    ultima modificacion con muchos comentarios

--- SGE/SGE.Aplicacion/Excepciones/AutorizacionException.cs
+++ SGE/SGE.Aplicacion/Excepciones/AutorizacionException.cs
@@ -2,6 +2,14 @@ namespace SGE.Aplicacion;
 
 class AutorizacionException
 {
-    // [ ]  La excepción AutorizacionException debe ser lanzada cuando un usuario intenta realizar una operación
-    //      para la cual no tiene los permisos necesarios.
+    // [ ]  La excepción AutorizacionException debe ser lanzada cuando un usuario intenta 
+    //realizar una operación para la cual no tiene los permisos necesarios.
+
+    //el mensaje lo dice todo, y se aplica siempre que el usuario quiera hacer algo 
+    //que no puede por su permiso.
+    public AutorizacionException (out string mensajeError) {
+        
+        mensajeError = $"No posee permiso para realizar esa operacion!";
+        
+    }
 }
--- SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
+++ SGE/SGE.Aplicacion/Validadores/ExpedienteValidador.cs
@@ -2,16 +2,22 @@
 
 public class ExpedienteValidador
 {
+
+    //las validaciones solo validan no dan los mensajes de error
+    //eso lo hace las excepciones
     public bool Validar(Expediente expediente, out string mensajeError)
     {
         mensajeError = "";
         if (expediente.IdUsuario <= 0)
         {
-            mensajeError = "El usuario es invalido.";
+            //mensajeError = "El usuario es invalido.";
+
+            throw new ValidacionException(mensajeError);       
         }
         else if (string.IsNullOrEmpty(expediente.Caratula))
         {
-            mensajeError = "La carátula de un expediente no puede estar vacía.";
+            //mensajeError = "La carátula de un expediente no puede estar vacía.";
+            throw new ValidacionException(mensajeError);
         }
         return (mensajeError == "");
     }
