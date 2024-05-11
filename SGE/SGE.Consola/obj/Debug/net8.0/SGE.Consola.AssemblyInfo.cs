d38c955ed3441d6d3c9e8a53468b94ee92b5bfbc
lapor <francolaporta10@gmail.com>
2024-05-11 19:14:50 -0300

    .

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abee2d67963d86e13baad6d33bd8129d8e80ef33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f2cf3c33ff3ba95adc07a94f35890de52f2646f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
