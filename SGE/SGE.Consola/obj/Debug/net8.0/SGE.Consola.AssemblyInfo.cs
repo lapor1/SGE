51f3b37dcbfe3d9d533fcda26a6a0bdbe35a49e2
Patricio Corbelleri <corbelleri.patricio@gmail.com>
2024-06-07 22:31:33 -0300

    pocos cambios

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+040deb1ca65a7ac23c1a6fb204f32ca1d1c8eb3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6115839ffb8cbd376d100529e127b7a82d4240f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
