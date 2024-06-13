2e755834dd64a2f5da28683457c3e22479ae73d1
lapor <francolaporta10@gmail.com>
2024-06-13 15:27:42 -0300

    agregue EF al SGE.Repositorios

--- SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
+++ SGE/SGE.Consola/obj/Debug/net8.0/SGE.Consola.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0a4028afdd7a40cce3888b2bfe774d09822b588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8570639a79452b9ee72ad724d4272d65cd80251")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Consola")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
