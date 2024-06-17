f21c53fc24c572ebb99f20284353561f4a21e126
lapor <francolaporta10@gmail.com>
2024-06-17 15:27:11 -0300

    servicio autorizacion de permisos

---
+++ SGE/SGE.Aplicacion/Servicios/ServicioAutorizacion.cs
@@ -0,0 +1,24 @@
+﻿using SGE.Aplicacion.Enumerativos;
+using SGE.Aplicacion.Interfaces;
+using SGE.Aplicacion.CasosDeUso;
+using SGE.Aplicacion.Entidades;
+
+namespace SGE.Aplicacion.Servicios;
+
+public class ServicioAutorizacion(IUsuarioRepositorio repoU) : IServicioAutorizacion
+{
+    public bool PoseeElPermiso(int IdUsuario, Permiso permiso) {
+        
+        var usuarioConsultarId = new CasoDeUsoUsuarioConsultaPorId(repoU);
+        Usuario? usuario = usuarioConsultarId.Ejecutar(IdUsuario);
+
+        if (usuario != null)
+        {
+            return usuario.ListaPermisos.Exists(p => p == permiso);
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
