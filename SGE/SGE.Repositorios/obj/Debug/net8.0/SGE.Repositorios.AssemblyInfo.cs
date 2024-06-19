e4d55dadb0a118c68a6e7e0d71a591339dd1706f
lapor <francolaporta10@gmail.com>
2024-06-19 02:05:36 -0300

    Limpieza

--- SGE/SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
+++ SGE/SGE.Repositorios/obj/Debug/net8.0/SGE.Repositorios.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2770ca707668e14abaef337bc2dae453a6ec16bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f69dc633e17217c08470eff273a6a5682196dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.Repositorios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
