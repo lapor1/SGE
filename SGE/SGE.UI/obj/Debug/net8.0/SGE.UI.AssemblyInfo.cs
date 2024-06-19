fb860c0dfe63595ed7eeb13e319b266bce414cd0
lapor <francolaporta10@gmail.com>
2024-06-19 18:19:42 -0300

    .

--- SGE/SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
+++ SGE/SGE.UI/obj/Debug/net8.0/SGE.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d55dadb0a118c68a6e7e0d71a591339dd1706f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4169d90b18c207fb96e38c62ba2e2c846bf391d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SGE.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
