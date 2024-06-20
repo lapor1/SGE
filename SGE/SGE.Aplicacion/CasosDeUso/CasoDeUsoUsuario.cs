33c5390eab5341e16c88edccee0f474007f3694b
lapor <francolaporta10@gmail.com>
2024-06-20 19:01:53 -0300

    cambios menores

--- SGE/SGE.Aplicacion/CasosDeUso/CasoDeUsoUsuario.cs
+++
@@ -1,8 +0,0 @@
-using SGE.Aplicacion.Interfaces;
-
-namespace SGE.Aplicacion.CasosDeUso;
-
-public abstract class CasosDeUso (IUsuarioRepositorio repoU)
-{
-    protected IUsuarioRepositorio Repositorio {get;} = repoU;
-}
